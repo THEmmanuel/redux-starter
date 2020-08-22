e6c46528491aafeb5bb16d53070b892bb7710b89
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-08-22 21:25:09 +0100

    Added switch body functions

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,20 @@
-const reducer =  (state = [], action) => {
-    switch (action.type){
-        case 'BUG_ADDED': 
+lastID = 0;
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case 'BUG_ADDED':
+            return [
+                ...state, {
+                    id: ++lastID,
+                    description: action.payload.description,
+                    isResolved: false
+                }
+            ]
+
         case 'BUG_REMOVED':
+            return state.filter(bug => bug.id !== action.payload.id)
+
+        default:
+            return state
     }
 }
